c6f6e9b33a37dd0572e898d506809d94a5234b16
jesusnunezxoco <jesusnunezxoco@gmail.com>
2020-10-22 02:37:19 -0500

    ...

--- fcc_node/myApp.js
+++ fcc_node/myApp.js
@@ -1,54 +1,45 @@
-
-var express = require('express');
+var express = require("express");
 var app = express();
 
-console.log("Hello World");
 
 // --> 7)  Mount the Logger middleware here
 
-
 // --> 11)  Mount the body-parser middleware  here
 
-
 /** 1) Meet the node console. */
-
+// console.log("Hello World");
 
 /** 2) A first working Express Server */
-
+// app.get("/", (req, res) => res.send("Hello Express"));
 
 /** 3) Serve an HTML file */
+app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 
 
 /** 4) Serve static assets  */
+app.use(express.static(__dirname + "/public"))
 
 
 /** 5) serve JSON on a specific route */
+app.get("/json", (req, res) => res.json({"message": "Hello json"}))
 
 
 /** 6) Use the .env file to configure the app */
- 
- 
+app.get("json", (req, res) => process.env.Message_STYLE=uppercase ? "Hello json".toUpperCase : "Hello Json")
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
 
-
 /** 8) Chaining middleware. A Time server */
 
-
 /** 9)  Get input from client - Route parameters */
-
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
 
-  
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
 
-
 /** 12) Get data form POST  */
-
-
 
 // This would be part of the basic setup of an Express app
 // but to allow FCC to run tests, the server is already active
@@ -56,4 +47,4 @@ console.log("Hello World");
 
 //---------- DO NOT EDIT BELOW THIS LINE --------------------
 
- module.exports = app;
+module.exports = app;

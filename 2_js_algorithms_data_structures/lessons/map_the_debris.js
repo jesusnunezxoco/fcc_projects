edb05d93ab6ebe49c4c850e74b40801cf9440faa
jesusnunezxoco <jesusnunezxoco@gmail.com>
2020-10-22 15:58:37 -0500

    arguments optional orbital period calculator complete

---
+++ 2_js_algorithms_data_structures/lessons/map_the_debris.js
@@ -0,0 +1,32 @@
+// Intermediate Algorithm Scripting: Map the Debris
+// Return a new array
+// Transform the elements' average altitude into their orbital periods (in seconds)
+// The array will contain objects in the format {name: 'name', avgAlt: avgAlt}.
+// You can read about orbital periods on Wikipedia.
+// The values should be rounded to the nearest whole number. The body being orbited is Earth.
+// The radius of the earth is 6367.4447 kilometers, and the GM value of earth is 398600.4418 km3s-2.
+
+function orbitalPeriod(arr) {
+  let GM = 398600.4418;
+  let earthRadius = 6367.4447;
+  let twoTimesPi = 2 * Math.PI;
+
+  return arr.map((obj) => {
+    let a = Math.pow(earthRadius + obj["avgAlt"], 3);
+
+    obj.orbitalPeriod = Math.round(twoTimesPi * Math.sqrt(a / GM));
+
+    delete obj["avgAlt"];
+    console.log(obj);
+    return obj;
+  });
+}
+
+console.log("1");
+orbitalPeriod([{ name: "sputnik", avgAlt: 35873.5553 }]);
+console.log("2");flaura pausni
+orbitalPeriod([
+  { name: "iss", avgAlt: 413.6 },
+  { name: "hubble", avgAlt: 556.7 },
+  { name: "moon", avgAlt: 378632.553 },
+]);
